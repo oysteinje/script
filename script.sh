2cea6d9cd6a155e29961de1239282d629200dfcc
ystrte <oystein.straete@basefarm.com>
2018-02-27 15:51:22 +0100

    commited script

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get -y update
+sudo apt-get -y install dotnet-sdk-2.1.4
+sudo apt-get -y install git
